384bbad095a38565e2a63f127f36076648271673
gr423_elmva <elokhov.2015@mail.ru>
2024-04-29 11:20:58 +0700

    3

--- js/main.js
+++ js/main.js
@@ -9,30 +9,47 @@ let app = new Vue({
     },
     methods: {
         addTask() {
-            this.needDoList.push(this.todo);
-        },
-        doCheck() {
-            for (let element in this.needDoList) {
-                this.needDoList.splice(this.needDoList[element], 1)
+            if (this.todo === "") {
+                alert("Введите значение")
+                return
             }
-            this.needDoListDo.push(this.todo)
-        },
-        doneCheck() {
-            for (let element in this.needDoListDo) {
-                this.needDoListDo.splice(this.needDoListDo[element], 1)
+            if (this.needDoList.length >= 3) {
+                alert("Больше 3 нельзя")
+            } else {
+                this.needDoList.push(this.todo);
+
             }
-            this.needDoListDone.push(this.todo)
+            this.todo.value = ""
         },
         schetRadio() {
-            this.schetchik += 1
-            console.log(this.schetchik)
-            if (this.schetchik >= 3) {
-                for (let element in this.needDoList) {
-                    this.needDoList.splice(this.needDoList[element], 1)
+            if (this.needDoListDo.length >= 5) {
+                alert("Больше 5 нельзя")
+                return
+            } else {
+                this.schetchik += 1
+                if (this.schetchik >= 3 && this.schetchik < 5) {
+                    this.needDoListDo.push(this.todo)
+                    this.schetchik = 0
+                    for (let element in this.needDoList) {
+                        this.needDoList.splice(this.needDoList[element], 1)
+                    }
+                    this.needDoListDo.push(this.todo)
+                    this.schetchik = 0
+
                 }
-                this.needDoListDo.push(this.todo)
+            }
+
+        },
+        schetRadioDo() {
+            this.schetchik += 1
+            if (this.schetchik >= 2) {
                 this.schetchik = 0
+                this.needDoListDone.push(this.todo)
+                for (let element in this.needDoListDo) {
+                    this.needDoListDo.splice(this.needDoListDo[element], 1)
+                }
+
             }
         }
     }
-    })
+})
