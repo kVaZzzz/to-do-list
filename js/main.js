78041ff0a60680677b3687ed7f1781509a2cee78
gr423_elmva <elokhov.2015@mail.ru>
2024-04-30 09:18:31 +0700

    теперь точно готово

--- js/main.js
+++ js/main.js
@@ -11,25 +11,25 @@ new Vue({
 
         }
     },
-    mounted(){
+    mounted() {
         if (localStorage.getItem('column1')) {
             try {
                 this.column1 = JSON.parse(localStorage.getItem('column1'));
-            } catch(e) {
+            } catch (e) {
                 localStorage.removeItem('column1');
             }
         }
         if (localStorage.getItem('column2')) {
             try {
                 this.column2 = JSON.parse(localStorage.getItem('column2'));
-            } catch(e) {
+            } catch (e) {
                 localStorage.removeItem('column2');
             }
         }
         if (localStorage.getItem('column3')) {
             try {
                 this.column3 = JSON.parse(localStorage.getItem('column3'));
-            } catch(e) {
+            } catch (e) {
                 localStorage.removeItem('column3');
             }
         }
@@ -37,16 +37,17 @@ new Vue({
     },
     methods: {
         handleCardPosition(card) {
-            const totalItems = card.items.length;
-            const completedItems = card.items.filter(item => item.completed).length;
+            totalItems = card.items.length;
+            completedItems = card.items.filter(item => item.completed).length;
 
             if (completedItems / totalItems >= 0.5 && this.column1.includes(card)) {
-                if (card.items.text === ''){console.log('asd')}
-                if(this.column2.length ===5 && completedItems / totalItems >= 0.5 && this.column1.includes(card) ){ this.check = false}
-                else {
+                if (this.column2.length === 5 && completedItems / totalItems >= 0.5 && this.column1.includes(card)) {
+                    this.check = false
+                } else {
                     this.column1.splice(this.column1.indexOf(card), 1);
                     this.column2.push(card);
-                    this.saveLocalStorage();}
+                    this.saveLocalStorage();
+                }
             } else if (completedItems / totalItems === 1 && this.column2.includes(card)) {
                 this.column2.splice(this.column2.indexOf(card), 1);
                 this.check = true
@@ -57,16 +58,16 @@ new Vue({
         },
         addCard() {
             if (this.newCardTitle !== '' && this.column1.length < 3) {
-                const newCard = {
+                newCard = {
                     id: Date.now(),
                     title: this.newCardTitle,
                     items: [
-                        { text: '', completed: false, editing: true },
-                        { text: '', completed: false, editing: true },
-                        { text: '', completed: false, editing: true }
+                        {text: '', completed: false, editing: true},
+                        {text: '', completed: false, editing: true},
+                        {text: '', completed: false, editing: true}
                     ],
                 };
-                if (this.newCardTitle !== '' ) {
+                if (this.newCardTitle !== '') {
                     this.column1.push(newCard);
                 }
 
@@ -78,14 +79,12 @@ new Vue({
             }
 
 
-
-
-
         },
-        addItem(card){
-            if(this.newItemText != '' && card.items.length <= 4){
+        addItem(card) {
+            if (this.newItemText != '' && card.items.length <= 4) {
                 card.items.push({id: Date.now(), text: this.newItemText, checked: false})
-                this.newItemText = '';}
+                this.newItemText = '';
+            }
         },
         saveLocalStorage() {
             const parsed = JSON.stringify(this.column1);
@@ -95,18 +94,5 @@ new Vue({
             localStorage.setItem('column2', parsed1);
             localStorage.setItem('column3', parsed2);
         },
-
-
-
-
-
     },
-    computed: {
-        columeTaskCount(){
-            return this.column2.length
-        },
-        TaskCheck(){
-            return this.check
-        }
-    }
 })
